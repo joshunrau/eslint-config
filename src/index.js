beb2ee742e8164881a4b7daf90a0ff5e4a23320b
joshunrau <contact@joshuaunrau.com>
2023-05-17 14:48:32 -0400

    start working on new config

--- src/index.js
+++ src/index.js
@@ -1,71 +1,31 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  env: {
-    es2022: true,
-    'shared-node-browser': true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['import', '@typescript-eslint'],
-  rules: {
-    'import/exports-last': 'warn',
-    'import/newline-after-import': 'warn',
-    'import/no-default-export': 'warn',
-    'import/order': [
-      'warn',
-      {
-        alphabetize: {
-          caseInsensitive: true,
-          order: 'asc'
-        },
-        'newlines-between': 'always'
-      }
-    ],
-    'sort-imports': [
-      'error',
-      {
-        ignoreDeclarationSort: true
-      }
-    ]
-  },
-  overrides: [
-    {
-      files: ['**/*.{ts,tsx}'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking'
-      ],
-      parser: '@typescript-eslint/parser',
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/restrict-plus-operands': 'off',
-        '@typescript-eslint/restrict-template-expressions': [
-          'error',
-          {
-            allowNumber: true,
-            allowBoolean: true
-          }
-        ],
-        'import/consistent-type-specifier-style': 'warn'
-      },
-      settings: {
-        'import/extensions': ['.ts'],
-        'import/parsers': {
-          '@typescript-eslint/parser': ['.ts']
-        },
-        'import/resolver': {
-          typescript: true
-        }
-      }
-    },
-  ]
+// @ts-check
+import js from '@eslint/js';
+
+/**
+ * @typedef { import('eslint').Linter.FlatConfig[] } Config
+ */
+
+/**
+ * @typedef {Object} Options
+ * @property {'esm' | 'cjs'} moduleType
+ */
+
+/** @type {Options} */
+const defaultOptions = {
+  moduleType: 'esm'
 };
+
+/**
+ * @param {Options} options
+ * @returns {Config}
+ */
+export function createConfig(options) {
+  return [
+    js.configs.recommended,
+    {
+      files: ['"**/*.js"']
+    }
+  ];
+}
+
+export default createConfig(defaultOptions);
