909795ca845798ad018be890c2d396a0b6e51daa
joshunrau <contact@joshuaunrau.com>
2023-05-30 11:49:09 -0400

    2.1.1

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import { createTsConfig } from './lib/ts.js';
  * @param {Object} options
  * @param {Object} options.typescript
  * @param {string} options.typescript.project
+ * @param {boolean} [options.typescript.jsx]
  */
 export function createConfig(options) {
   return [
