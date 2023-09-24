2cb17ce14201d8d40674b16177f57e4e333f0300
joshunrau <contact@joshuaunrau.com>
2023-09-24 16:22:21 -0400

    use ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural';
+import globals from 'globals';
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+console.log(reactPlugin);
---
+++ src/typing.d.ts
@@ -0,0 +1,5 @@
+
+// declare module 'eslint-plugin-perfectionist/configs/recommended-natural' {
+//   export const rules: Record<string, any[]>;
+//   export const plugins: Record<string, any>;
+// }
