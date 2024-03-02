49a98c6eed49931f225a46d60f7ccfe5f088d804
joshunrau <contact@joshuaunrau.com>
2024-03-02 18:54:32 -0500

    remove dead code

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,3 @@
 import { config } from './src/index.js';
 
 export default config();
-// import path from 'path';
-// import url from 'url';
-
-// import { createConfig } from './src/index.js';
-
-// const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
-// export default createConfig({
-//   base: {
-//     env: 'node'
-//   },
-//   ts: {
-//     project: path.resolve(__dirname, 'tsconfig.json')
-//   }
-// });
