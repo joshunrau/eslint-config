b051486169597185ae8f6eda32f8aeedba521d90
joshunrau <contact@joshuaunrau.com>
2024-03-02 21:33:12 -0500

    update json

--- src/configs/json.js
+++ src/configs/json.js
@@ -17,32 +17,7 @@ export const jsonConfig = async ({ json }) => {
         jsonc: plugin
       },
       rules: {
-        'jsonc/no-bigint-literals': 'error',
-        'jsonc/no-binary-expression': 'error',
-        'jsonc/no-binary-numeric-literals': 'error',
-        'jsonc/no-dupe-keys': 'error',
-        'jsonc/no-escape-sequence-in-identifier': 'error',
-        'jsonc/no-floating-decimal': 'error',
-        'jsonc/no-hexadecimal-numeric-literals': 'error',
-        'jsonc/no-infinity': 'error',
-        'jsonc/no-multi-str': 'error',
-        'jsonc/no-nan': 'error',
-        'jsonc/no-number-props': 'error',
-        'jsonc/no-numeric-separators': 'error',
-        'jsonc/no-octal': 'error',
-        'jsonc/no-octal-escape': 'error',
-        'jsonc/no-octal-numeric-literals': 'error',
-        'jsonc/no-parenthesized': 'error',
-        'jsonc/no-plus-sign': 'error',
-        'jsonc/no-regexp-literals': 'error',
-        'jsonc/no-sparse-arrays': 'error',
-        'jsonc/no-template-literals': 'error',
-        'jsonc/no-undefined-value': 'error',
-        'jsonc/no-unicode-codepoint-escapes': 'error',
-        'jsonc/no-useless-escape': 'error',
-        'jsonc/space-unary-ops': 'error',
-        'jsonc/valid-json-number': 'error',
-        'jsonc/vue-custom-block/no-parsing-error': 'error',
+        'jsonc/key-name-casing': ['warn', { camelCase: true }],
         'no-unused-expressions': 'off',
         'no-unused-vars': 'off'
       }
@@ -53,14 +28,14 @@ export const jsonConfig = async ({ json }) => {
       files: ['**/package.json'],
       rules: {
         'jsonc/sort-array-values': [
-          'error',
+          'warn',
           {
             order: { type: 'asc' },
             pathPattern: '^files$'
           }
         ],
         'jsonc/sort-keys': [
-          'error',
+          'warn',
           {
             order: [
               'publisher',
